6d52da8c23d394047c0f9973d0ae1654be3bcac1
Anastasiia Fokina <stasechka11@yandex.ru>
2024-01-03 11:02:11 +0300

    Initial realization of tasks objects

---
+++ src/main/java/ru/netology/Epic.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+}
