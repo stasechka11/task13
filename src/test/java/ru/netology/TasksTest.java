af62421166a845ec6d27b8199c0a6b57e82cb197
Anastasiia Fokina <stasechka11@yandex.ru>
2024-01-03 17:17:47 +0300

    Added more tests for method matches

--- src/test/java/ru/netology/TasksTest.java
+++ src/test/java/ru/netology/TasksTest.java
@@ -13,6 +13,14 @@ public class TasksTest {
     }
 
     @Test
+    public void shouldFindSimpleTaskByQueryPartialString() {
+        SimpleTask simpleTask = new SimpleTask(34, "Сделать доклад");
+        String query = "доклад";
+
+        Assertions.assertTrue(simpleTask.matches(query));
+    }
+
+    @Test
     public void shouldFindSimpleTaskByQueryNegative() {
         SimpleTask simpleTask = new SimpleTask(2, "Изучить тему Наследование");
         String query = "Изучить тему Конструкторы";
@@ -25,6 +33,15 @@ public class TasksTest {
         String[] subtasks = {"Subtask 1", "Subtask 2", "Subtask 3"};
         Epic epic = new Epic(3, subtasks);
         String query = "Subtask 2";
+
+        Assertions.assertTrue(epic.matches(query));
+    }
+
+    @Test
+    public void shouldFindEpicByQueryPartialString() {
+        String[] subtasks = {"Subtask 1", "Subtask 2", "Subtask 3"};
+        Epic epic = new Epic(345, subtasks);
+        String query = "2";
 
         Assertions.assertTrue(epic.matches(query));
     }
@@ -50,5 +67,45 @@ public class TasksTest {
         String query = "Наследование классов";
 
         Assertions.assertTrue(meeting.matches(query));
+    }
+
+    @Test
+    public void shouldFindMeetingByQueryInProjectPositive() {
+        Meeting meeting = new Meeting(
+                6,
+                "Исключения",
+                "Изучение Java",
+                "08.01.2024");
+
+        String query = "Изучение Java";
+
+        Assertions.assertTrue(meeting.matches(query));
+    }
+
+    @Test
+    public void shouldFindMeetingByQueryPartialString() {
+        Meeting meeting = new Meeting(
+                22,
+                "Массивы",
+                "Изучение Java",
+                "14.11.2023");
+
+        String query = "Java";
+
+        Assertions.assertTrue(meeting.matches(query));
+    }
+
+    @Test
+    public void shouldFindMeetingByQueryNegative() {
+        Meeting meeting = new Meeting(
+                12,
+                "Ветки",
+                "Git",
+                "12.12.2023");
+
+        String query = "12.12.2023";
+
+        Assertions.assertFalse(meeting.matches(query));
+
     }
 }
